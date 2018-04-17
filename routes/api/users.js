30cd1be60794e334cbb852d27482f8e7c9d418d2
TurtleWolfe <JonPohlner@gmail.com>
2018-04-17 11:32:30 -0400

    validation, video 14

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//  Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+
 //Load User model
 const User = require("../../models/User");
 
@@ -22,13 +25,19 @@ router.get("/test", (req, res) =>
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({
     email: req.body.email
   }).then(user => {
     if (user) {
-      return res.status(400).json({
-        email: "This Email is already in the system"
-      });
+      errors.email = "This Email is already in the system";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", // size
