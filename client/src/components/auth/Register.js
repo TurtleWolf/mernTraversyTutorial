29784a019d5b0da815d4d6a43ce7eba85dcaf62d
TurtleWolfe <jonpohlner@gmail.com>
2018-05-01 14:29:56 -0400

    S9, L46 Spinner Component & Dashboard Start

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
-//import axios from "axios";
-import classnames from "classnames";
+// import axios from "axios";
+// import classnames from "classnames";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 import TextFieldGroup from "../common/TextFieldGroup";
